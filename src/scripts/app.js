1c25ca1d77cc72ae2f7f2ebda36e3f9df81cf58d
Donaldo Lara-Reyes <bonelessdarkmatter_code@protonmail.com>
2024-02-12 19:43:31 -0500

    add menu button animation when clicked

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,6 @@
-const testing = () => {
-	console.log('hello world');
-};
+const menuButton = document.getElementById('hamburger-menu-button');
+menuButton.addEventListener('click', function () {
+	menuButton.classList.contains('is-active')
+		? menuButton.classList.remove('is-active')
+		: menuButton.classList.add('is-active');
+});
